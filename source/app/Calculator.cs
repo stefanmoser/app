b487053212767b2e6b8425f579b7b814bb91bc1c
Stefan Moser <stefanm@iqmetrix.com>
2011-11-22 16:53:27 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,15 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if (first < 0 || second < 0)
+        {
+            throw new ArgumentException();
+        }
       return first + second;
     }
   }
