88789d8f93f745b3988f0202317bdbb1c3c87b09
Stefan Moser <stefanm@iqmetrix.com>
2011-11-23 13:02:44 -0600

    Pushing new changes

---
+++ source/app.specs/CommandSpecs.cs
@@ -0,0 +1,43 @@
+﻿using Machine.Specifications;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{
+    public class CommandSpecs
+    {
+        public class concern : Observes<IProcessOneRequest,
+            Command>
+        {
+        }
+
+        public class when_checking_if_a_command_can_handle_a_request : concern
+        {
+            Establish e = () =>
+            {
+                the_request = fake.an<IContainRequestInformation>();
+                request_matcher = depends.on<IMatchRequests>();
+            };
+
+            private Because b = () =>
+                                can_handle_the_request = sut.can_handle(the_request);
+
+            It should_check_if_the_request_matches = () =>
+                    request_matcher.received(x => x.matches_request(the_request));
+
+            private static IContainRequestInformation the_request;
+            private static IMatchRequests request_matcher;
+            private static bool can_handle_the_request;
+
+            public class and_the_command_can_handle_the_request : when_checking_if_a_command_can_handle_a_request
+            {
+                Establish e = () => 
+                    request_matcher.setup(x => x.matches_request(the_request)).Return(true);
+                
+                It should_be_able_to_handle_the_request = () =>
+                    can_handle_the_request.ShouldBeTrue();
+            }
+        }
+    }
+}
---
+++ source/app/web/core/IMatchRequests.cs
@@ -0,0 +1,7 @@
+﻿namespace app.web.core
+{
+    public interface IMatchRequests
+    {
+        bool matches_request(IContainRequestInformation the_request);
+    }
+}
