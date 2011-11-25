44036260e0ba5baaf2ca8273edd0e44ea1daa992
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-11-25 12:49:23 -0700

    Updated

--- source/app/tasks/startup/Startup.cs
+++ source/app/tasks/startup/Startup.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 using System.Web;
 using System.Web.Compilation;
 using app.utility.containers;
-using System.Linq;
 using app.utility.containers.basic;
-using app.web.application;
 using app.web.core;
 using app.web.core.aspnet;
 using app.web.core.aspnet.stubs;
@@ -20,7 +17,7 @@ namespace app.tasks.startup
 
     public static void run()
     {
-      dependencies =  new Dictionary<Type, ICreateADependency>();
+      dependencies = new Dictionary<Type, ICreateADependency>();
       IFetchDependencies container = new DependencyContainer(dependencies);
       ContainerFacadeResolution facade_resolution = () => container;
       Container.facade_resolution = facade_resolution;
@@ -30,22 +27,31 @@ namespace app.tasks.startup
 
     static void wire_everything_up()
     {
-		dependencies.Add(typeof(IBuildRequestMatchers), new SimpleFactory(() => new RequestMatchBuilder()));
+      dependencies.Add(typeof(IBuildRequestMatchers), new SimpleFactory(() => new RequestMatchBuilder()));
 
-		dependencies.Add(typeof(MatchBuilderFactory), new SimpleFactory(() => (MatchBuilderFactory)Container.fetch.an<IBuildRequestMatchers>));
+      dependencies.Add(typeof(MatchBuilderFactory),
+                       new SimpleFactory(() => (MatchBuilderFactory) Container.fetch.an<IBuildRequestMatchers>));
 
-		dependencies.Add(typeof(IFindPathsToViews), new SimpleFactory(Stub.with<StubPathRegistry>));
+      dependencies.Add(typeof(IFindPathsToViews), new SimpleFactory(Stub.with<StubPathRegistry>));
 
-		dependencies.Add(typeof(ICreateAResponse), new SimpleFactory(() => new PageFactory(Container.fetch.an<IFindPathsToViews>(), BuildManager.CreateInstanceFromVirtualPath)));
+      dependencies.Add(typeof(ICreateAResponse),
+                       new SimpleFactory(
+                         () =>
+                           new PageFactory(Container.fetch.an<IFindPathsToViews>(),
+                                           BuildManager.CreateInstanceFromVirtualPath)));
 
-		dependencies.Add(typeof(IDisplayReportModels), new SimpleFactory(() => new WebResponseEngine(Container.fetch.an<ICreateAResponse>(), () => HttpContext.Current)));
+      dependencies.Add(typeof(IDisplayReportModels),
+                       new SimpleFactory(
+                         () => new WebResponseEngine(Container.fetch.an<ICreateAResponse>(), () => HttpContext.Current)));
 
-		dependencies.Add(typeof(IFindCommands), new SimpleFactory(() => new CommandRegistry(Stub.with<StubSetOfCommands>(), Stub.with<StubMissingCommand>())));
+      dependencies.Add(typeof(IFindCommands),
+                       new SimpleFactory(
+                         () => new CommandRegistry(Stub.with<StubSetOfCommands>(), Stub.with<StubMissingCommand>())));
 
-		dependencies.Add(typeof(ICreateRequests), new SimpleFactory(Stub.with<StubRequestFactory>));
+      dependencies.Add(typeof(ICreateRequests), new SimpleFactory(Stub.with<StubRequestFactory>));
 
-    	var fc = new FrontController(Container.fetch.an<IFindCommands>());
-		dependencies.Add(typeof(IProcessRequests), new SimpleFactory(() => fc));
+      var fc = new FrontController(Container.fetch.an<IFindCommands>());
+      dependencies.Add(typeof(IProcessRequests), new SimpleFactory(() => fc));
     }
   }
 }
