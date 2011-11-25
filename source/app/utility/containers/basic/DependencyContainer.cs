b37bf39ce2803ec920fb7b88e91882e202e4afb3
Schonhoffer <andrews@iqmetrix.com>
2011-11-25 11:46:18 -0600

    Pushing new changes

--- source/app/utility/containers/basic/DependencyContainer.cs
+++ source/app/utility/containers/basic/DependencyContainer.cs
@@ -1,17 +1,26 @@
-﻿namespace app.utility.containers.basic
+﻿using System;
+
+namespace app.utility.containers.basic
 {
   public class DependencyContainer : IFetchDependencies
   {
-    IFindFactoriesForDependencies dependencies;
+	IFindFactoriesForDependencies dependencies;
 
-    public DependencyContainer(IFindFactoriesForDependencies dependencies)
-    {
-      this.dependencies = dependencies;
-    }
+	public DependencyContainer(IFindFactoriesForDependencies dependencies)
+	{
+	  this.dependencies = dependencies;
+	}
 
-    public Dependency an<Dependency>()
-    {
-      return (Dependency)dependencies.get_the_factory_that_can_create(typeof(Dependency)).create();
-    }
+	public Dependency an<Dependency>()
+	{
+		try
+		{
+			return (Dependency)dependencies.get_the_factory_that_can_create(typeof(Dependency)).create();
+		}
+		catch (Exception e)
+		{
+			throw new DependencyCreationException("Cannot create type", typeof(Dependency), e);
+		}
+	}
   }
 }
--- source/app/utility/containers/basic/DependencyCreationException.cs
+++ source/app/utility/containers/basic/DependencyCreationException.cs
@@ -4,9 +4,12 @@ namespace app.utility.containers.basic
 {
   public class DependencyCreationException : Exception
   {
-    public Type type_that_could_not_be_created
-    {
-      get { throw new System.NotImplementedException(); }
-    }
+
+	  public DependencyCreationException(string message, Type bad_type, Exception e) :base(message, e)
+	  {
+		this.type_that_could_not_be_created = bad_type;
+	  }
+
+	  public Type type_that_could_not_be_created { get; private set; }
   }
 }
